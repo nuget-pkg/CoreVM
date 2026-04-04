eb091b27fc2f91002fafa6d48c0bee0e31f78f16
javacommons <javacommons@gmail.com>
2026-04-04 18:59:47 +0900

    .

--- ndl.main.cs
+++ ndl.main.cs
@@ -1,4 +1,14 @@
-//css_nuget NugetDownloader
+// //css_nuget NugetDownloader
+//css_dir NugetWorker
+
+//css_nuget log4net@2.0.8
+//css_nuget Nuget.Packagemanagement@4.8.0
+//css_nuget Nuget.Projectmodel@4.8.0
+//css_nuget Nuget.Protocol@4.8.0
+//css_nuget Nuget.resolver@4.8.0
+//css_nuget System.Runtime.Loader@4.3.0
+//css_nuget System.Runtime.Serialization.Json@4.3.0
+
 //css_nuget EasyObject
 using NugetWorker;
 using static Global.EasyObject;
