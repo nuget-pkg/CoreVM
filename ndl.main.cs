70dedc1d61613b644ce3caed3f6d6544f185fd71
javacommons <javacommons@gmail.com>
2026-04-04 19:31:02 +0900

    .

--- ndl.main.cs
+++ ndl.main.cs
@@ -1,25 +1,33 @@
-// //css_nuget NugetDownloader
-//css_dir NugetWorker
-
-//css_nuget log4net@2.0.8
-//css_nuget Nuget.Packagemanagement@4.8.0
-//css_nuget Nuget.Projectmodel@4.8.0
-//css_nuget Nuget.Protocol@4.8.0
-//css_nuget Nuget.resolver@4.8.0
-//css_nuget System.Runtime.Loader@4.3.0
-//css_nuget System.Runtime.Serialization.Json@4.3.0
-
+//css_nuget NuGet.Protocol
 //css_nuget EasyObject
-using NugetWorker;
+using NuGet.Common;
+using NuGet.Configuration;
+using NuGet.Protocol;
+using NuGet.Protocol.Core.Types;
+using NuGet.Versioning;
+using System.IO;
+using System.Threading;
 using static Global.EasyObject;
-//using NugetEngine;
-Log("hello");
+
 try {
-    string packageName="Newtonsoft.json";
-    string version="10.2.1.0";
-    NugetEngine nugetEngine = new NugetEngine();
-    nugetEngine.GetPackage(packageName, version).Wait();
-    Log("end");
+    PackageSource localSource = new PackageSource(@"C:\LocalSource");
+    SourceRepository localRepository = Repository.Factory.GetCoreV3(localSource);
+    SourceRepository repository = Repository.Factory.GetCoreV3("https://api.nuget.org/v3/index.json"); FindPackageByIdResource resource = await repository.GetResourceAsync<FindPackageByIdResource>();
+    string packageId = "Newtonsoft.Json";
+    NuGetVersion packageVersion = new NuGetVersion("12.0.1");
+    using MemoryStream packageStream = new MemoryStream();
+    await resource.CopyNupkgToStreamAsync(
+        packageId,
+        packageVersion,
+        packageStream,
+        new SourceCacheContext(),
+        NullLogger.Instance,
+        CancellationToken.None);
+    packageStream.Seek(0, SeekOrigin.Begin);
+    Log(packageStream.Length);
+    ExpectTrue(packageStream.Length > 0, "Package stream should not be empty.");
+    using FileStream fileStream = new FileStream(@"C:\home17\tmp\Newtonsoft.Json.nupkg", FileMode.Create, FileAccess.Write);
+   packageStream.CopyTo(fileStream);
 } catch(System.Exception ex) {
     Abort(ex);
 }
